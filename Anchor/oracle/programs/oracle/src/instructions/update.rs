25aa87626469705ecf30a1b9b91040c774deb127
Babybluess <21021544@vnu.edu.vn>
2026-03-25 15:39:41 +0700

    feat: Add oracle exercise in native rust and anchor

---
+++ Anchor/oracle/programs/oracle/src/instructions/update.rs
@@ -0,0 +1,19 @@
+use anchor_lang::prelude::*;
+
+use crate::state::Oracle;
+
+#[derive(Accounts)]
+pub struct Update<'info> {
+    // Write your code here
+    pub owner: Signer<'info>,
+    // Write your code here
+    #[account(mut, constraint = oracle.owner == owner.key())]
+    // Check oracle.owner == owner
+    // oracle is deserialized to Oracle struct
+    pub oracle: Account<'info, Oracle>,
+}
+
+pub fn update(ctx: Context<Update>, price: u64) -> Result<()> {
+    ctx.accounts.oracle.price = price;
+    Ok(())
+}
