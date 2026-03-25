ab047f99c4b94b95fbf82aa213602da996248886
Babybluess <21021544@vnu.edu.vn>
2026-03-25 10:57:55 +0700

    feat: Add hello_solana exercise in native rust

---
+++ Native/hello_solana/src/lib.rs
@@ -0,0 +1,20 @@
+use solana_program::{
+    account_info::AccountInfo,
+    entrypoint,
+    entrypoint::ProgramResult,
+    pubkey::Pubkey,
+    msg,
+};
+
+// Declare and export the program's entrypoint
+entrypoint!(process_instruction);
+
+// Program entrypoint's implementation
+pub fn process_instruction(
+    program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    instruction_data: &[u8],
+) -> ProgramResult {
+    msg!("Hello, Solana!");
+    Ok(())
+}
